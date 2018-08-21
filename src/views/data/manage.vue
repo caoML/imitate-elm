c5a580a4431be93581530f8d0f7f9feade0ee822
LiuHao <1453535553@qq.com>
2018-08-21 23:11:54 +0800

    add manage chart

--- src/views/data/manage.vue
+++ src/views/data/manage.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <h1>数据统计</h1>
-    <button @click="clickme">click</button>
     <el-row :gutter="20">
       <el-col :span="3">
         <div class="grid-content today_head data-title">当日数据:</div>
@@ -20,7 +19,7 @@
       </el-col>
       <el-col :span="3">
         <div class="grid-content bg-purple">
-          <span>{{newi['test']}}</span>
+          <span>{{newin['admin']}}</span>
           <span>新增管理员</span>
         </div>
       </el-col>
@@ -48,10 +47,12 @@
         </div>
       </el-col>
     </el-row>
+    <div ref="main" style="height: 700px;"></div>
   </div>
 </template>
 
 <script>
+  import echarts from 'echarts'
   export default {
     data() {
       return {
@@ -80,6 +81,7 @@
           }
         )
       })
+      this.initCharts()
     },
     methods: {
       change() {
@@ -96,8 +98,71 @@
           (day < 10 ? '0' + day : day)
         return newTime
       },
-      clickme() {
-        console.log(this.newin)
+      initCharts() {
+        const option = {
+          title: {
+            text: '折线图堆叠'
+          },
+          tooltip: {
+            trigger: 'axis'
+          },
+          legend: {
+            data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [
+            {
+              name: '邮件营销',
+              type: 'line',
+              stack: '总量',
+              data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+              name: '联盟广告',
+              type: 'line',
+              stack: '总量',
+              data: [220, 182, 191, 234, 290, 330, 310]
+            },
+            {
+              name: '视频广告',
+              type: 'line',
+              stack: '总量',
+              data: [150, 232, 201, 154, 190, 330, 410]
+            },
+            {
+              name: '直接访问',
+              type: 'line',
+              stack: '总量',
+              data: [320, 332, 301, 334, 390, 330, 320]
+            },
+            {
+              name: '搜索引擎',
+              type: 'line',
+              stack: '总量',
+              data: [820, 932, 901, 934, 1290, 1330, 1320]
+            }
+          ]
+        }
+        var myChart = echarts.init(this.$refs.main)
+        myChart.setOption(option)
       }
     }
   }

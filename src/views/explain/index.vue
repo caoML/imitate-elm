d63c28a1550eb81017dae0e2a1f5915b53021c48
cml <972121471@qq.com>
2018-08-23 14:57:28 +0800

    add explain

--- src/views/explain/index.vue
+++ src/views/explain/index.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>node-elm后台管理系统
+    <div>web管理台系统
 
-第一次登录的用户自动注册成为普通管理员
+后台接口来自https://github.com/bailicangdu/node-elm/blob/master/API.md
+第一次登录的用户自动注册成为普通管理员，
+其中大部分数据不可更改
 
-普通管理员可以添加，修改信息
-
-超级管理员可以删除信息</div>
+</div>
 </template>
 
 <script>
